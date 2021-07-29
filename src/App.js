1f838eb2c9c860f2063f3b2f3b7e87f20bac6e95
alaspuresujay <alaspuresujay@gmail.com>
2021-07-29 19:59:37 +0530

    :rewind: starting over

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+
 function App() {
-	return <div className='app'></div>;
+	return (
+		<Router>
+			<Navbar />
+			<Switch>
+				<Route path='/' exact component={Home} />
+				{/* <Route path='/services' component={Services} />
+				<Route path='/products' component={Products} />
+				<Route path='/sign-up' component={SignUp} /> */}
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/constant.js
@@ -0,0 +1,9 @@
+export const colors = {
+	primary: '#A179AF',
+	secondary: '#564B64',
+	background: '#EDEAEA',
+	backgroundSecondary: '#3E3547',
+	text: '#3E3547',
+	textSecondary: '#ABA6A6',
+	white: '#FBFBFB',
+};
