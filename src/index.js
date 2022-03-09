46218fdddf8c21435ff724227192e6d0e89673aa
alaspuresujay <alapsuresujay@gmail.com>
2022-03-09 21:18:06 +0530

    :wrench:

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <HashRouter basename="/">
+        <App />
+      </HashRouter>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
